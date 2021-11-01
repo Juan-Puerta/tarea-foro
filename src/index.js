8c3eee5ce95dd16bd017bab18df6c8ad3d3983e9
Fabian Portilla <fdportillam1@gmail.com>
2021-11-01 14:57:27 -0500

    :+1:

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "./components/Mensaje/Mensaje.css"
 import { AppContextWrapper } from "./store/AppContext";
 
 ReactDOM.render(
